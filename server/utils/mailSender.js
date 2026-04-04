b96f0c0de0bbeec98c5aeab7a08a5276137bdff9
saksham021012 <sakshamgoyal2k4@gmail.com>
2026-04-04 23:27:14 +0530

    mail port change

--- server/utils/mailSender.js
+++ server/utils/mailSender.js
@@ -3,9 +3,9 @@ const nodemailer = require("nodemailer");
 const mailSender = async (email, title, body) => {
     try {
         let transporter = nodemailer.createTransport({
-            host: process.env.MAIL_HOST, 
-            port: 587,  
-            secure: false, 
+            host: process.env.MAIL_HOST,
+            port: 465,
+            secure: true,
             auth: {
                 user: process.env.MAIL_USER,
                 pass: process.env.MAIL_PASS,
@@ -13,7 +13,7 @@ const mailSender = async (email, title, body) => {
         });
 
         let info = await transporter.sendMail({
-            from: `"TravelSync" <${process.env.MAIL_USER}>`, 
+            from: `"TravelSync" <${process.env.MAIL_USER}>`,
             to: email,
             subject: title,
             html: body,
